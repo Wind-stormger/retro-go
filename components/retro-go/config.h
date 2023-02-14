84f86bce469310ccf953a8012adf97e419a8c29d
Wind-stormger <storm-wind@foxmail.com>
2023-02-14 16:27:28 +0800

    fix s3 error

--- components/retro-go/config.h
+++ components/retro-go/config.h
@@ -12,6 +12,8 @@
 #include "targets/mrgc-gbm/config.h"
 #elif defined(RG_TARGET_ESPLAY_MICRO)
 #include "targets/esplay-micro/config.h"
+#elif defined(RG_TARGET_ESPLAY_S3)
+#include "targets/esplay-s3/config.h"
 #else
 #warning "No target defined. Defaulting to ODROID-GO."
 #include "targets/odroid-go/config.h"
